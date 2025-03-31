1f055d5f1b33ed8d6a9a1e9e9a9dd3f75f41e552
jamie 🌨️ <jamie@wisp.bio>
2025-03-31 13:59:35 -0400

    chore: fix lint errors in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,51 @@
-import antfu from "@antfu/eslint-config";
+import antfu from '@antfu/eslint-config'
 
 export default antfu({
   typescript: {
-    tsconfigPath: "./tsconfig.json",
+    tsconfigPath: './tsconfig.json',
     overrides: {
-      "ts/no-floating-promises": "error",
-      "ts/consistent-type-imports": "error",
+      'ts/no-floating-promises': 'error',
+      'ts/consistent-type-imports': 'error',
     },
   },
 
-  ignores: ["**/node_modules/**", "**/dist/**"],
+  ignores: ['**/node_modules/**', '**/dist/**'],
 
   rules: {
-    "style/quotes": ["error", "single"],
-    "antfu/if-newline": "off",
-    "node/prefer-global/process": "off",
-    "perfectionist/sort-imports": "error",
-    "style/multiline-ternary": ["error", "always-multiline"],
-    "style/padding-line-between-statements": [
-      "error",
-      { blankLine: "always", prev: "var", next: "return" },
-    ],
-    "style/object-curly-newline": [
-      "error",
+    'style/quotes': ['error', 'single'],
+    'antfu/if-newline': 'off',
+    'node/prefer-global/process': 'off',
+    'perfectionist/sort-imports': 'error',
+    'style/multiline-ternary': ['error', 'always-multiline'],
+    'style/padding-line-between-statements': [
+      'error',
       {
-        ObjectExpression: "always",
-        ObjectPattern: { multiline: true },
-        ImportDeclaration: "never",
-        ExportDeclaration: { multiline: true, minProperties: 3 },
+        blankLine: 'always',
+        prev: 'var',
+        next: 'return',
       },
     ],
-    "style/indent": ["error", 2],
-    "style/no-multiple-empty-lines": ["error", { max: 1, maxBOF: 0 }],
-    "style/space-in-parens": ["error", "never"],
-    "style/function-paren-newline": ["error", "multiline"],
-    "ts/strict-boolean-expressions": "off",
+    'style/object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: 'never',
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'style/indent': ['error', 2],
+    'style/no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxBOF: 0,
+    }],
+    'style/space-in-parens': ['error', 'never'],
+    'style/function-paren-newline': ['error', 'multiline'],
+    'ts/strict-boolean-expressions': 'off',
   },
-});
+})
