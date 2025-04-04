a665625972b4f29682aeffe76dc1addef7be151e
jamie 🌨️ <jamie@wisp.bio>
2025-04-04 16:35:30 -0400

    chore: update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,45 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+
   typescript: {
     tsconfigPath: './tsconfig.json',
     overrides: {
       'ts/no-floating-promises': 'error',
       'ts/consistent-type-imports': 'error',
+
     },
   },
 
-  ignores: ['**/node_modules/**', '**/dist/**'],
+  ignores: [
+    '**/node_modules/**',
+    '**/dist/**',
+
+  ],
 
   rules: {
+
     'style/quotes': ['error', 'single'],
     'antfu/if-newline': 'off',
     'node/prefer-global/process': 'off',
     'perfectionist/sort-imports': 'error',
     'style/multiline-ternary': ['error', 'always-multiline'],
-    'style/padding-line-between-statements': [
-      'error',
-      {
-        blankLine: 'always',
-        prev: 'var',
-        next: 'return',
+    'style/padding-line-between-statements': ['error', {
+      blankLine: 'always',
+      prev: 'var',
+      next: 'return',
+    }],
+    'style/object-curly-newline': ['error', {
+      ObjectExpression: 'always',
+      ObjectPattern: {
+        multiline: true,
       },
-    ],
-    'style/object-curly-newline': [
-      'error',
-      {
-        ObjectExpression: 'always',
-        ObjectPattern: {
-          multiline: true,
-        },
-        ImportDeclaration: 'never',
-        ExportDeclaration: {
-          multiline: true,
-          minProperties: 3,
-        },
+      ImportDeclaration: 'never',
+      ExportDeclaration: {
+        multiline: true,
+        minProperties: 3,
       },
-    ],
+    }],
     'style/indent': ['error', 2],
     'style/no-multiple-empty-lines': ['error', {
       max: 1,
@@ -46,6 +47,11 @@ export default antfu({
     }],
     'style/space-in-parens': ['error', 'never'],
     'style/function-paren-newline': ['error', 'multiline'],
+    'arrow-body-style': ['warn', 'as-needed', {
+      requireReturnForObjectLiteral: true,
+    }],
+    'object-shorthand': ['warn', 'always'],
     'ts/strict-boolean-expressions': 'off',
   },
+
 })
