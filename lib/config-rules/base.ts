9b770a9291af397b7260660f2d868d5d58de90fd
jamie 🌨️ <jamie@wisp.bio>
2025-04-04 16:07:46 -0400

    refactor: update base rules

--- lib/config-rules/base.ts
+++ lib/config-rules/base.ts
@@ -40,6 +40,10 @@ export const baseRules = formatRules({
 
   'style/space-in-parens': ['error', 'never'],
   'style/function-paren-newline': ['error', 'multiline'],
-
+    'arrow-body-style': ['warn', 'as-needed', {
+      requireReturnForObjectLiteral: true,
+    }],
+    'curly': ['warn', 'multi'],
+    'object-shorthand': ['warn', 'always'],
   'ts/strict-boolean-expressions': 'off',
 })
